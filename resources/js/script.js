a7d28d308362c8716f483f1ada698432355470b5
Tyler Patton <tylerp@inmotionhosting.com>
2015-08-06 19:28:34 -0700

    Finish adding and styling sections
    Add search domain form in forms section
    Add compad tooltips functionality
    Add more filler content

--- resources/js/script.js
+++ resources/js/script.js
@@ -37,6 +37,58 @@ var $ = jQuery = require('jQuery'),
                 }
             });
         });
+
+        // Compads
+        $('.comp_adv_trigger').tooltip();
+
+        // Seach Domain Form Code
+        $(function(){
+            var domain_search_obj = $('#domain-search-button');
+            var input_obj = $('input#domain-search-input');
+            var valid_exts = ['com', 'net', 'org', 'info', 'biz', 'us'];
+            input_obj.keydown(function(e){
+                if (e.keyCode == 13) {
+                    domain_search_obj.first().trigger('click');
+                    return false;
+                }
+                return true;
+            });
+            domain_search_obj.click(function(){
+                var input_text = input_obj.val();
+                var responseSec = $('#domain-input-wrap'),
+                    resContainer = $('.res-container');
+                responseSec.removeClass('error success');
+                if (input_text == '') {
+                    // Enter a Domain
+                    resContainer.html('Enter a Domain');
+                    responseSec.addClass('error');
+                    input_obj.trigger('focus').select();
+                } else if (input_text.match(/^[a-z0-9]([a-z0-9\-]*[a-z0-9])*\.([a-z]{2,4}\.)*[a-z]{2,4}$/i) == null) {
+                    // Not a Domain
+                    resContainer.html('Not a Domain');
+                    responseSec.addClass('error');
+                    input_obj.trigger('focus').select();
+                } else if (valid_exts.indexOf((input_text.split('.'))[1]) == -1) {
+                    // Invalid TLD
+                    resContainer.html('Invalid TLD');
+                    responseSec.addClass('error');
+                    input_obj.trigger('focus').select();
+                } else {
+                    input_obj.prop('readonly', true);
+                    resContainer.html('Loading...');
+                    responseSec.addClass('loading');
+                    $('.loading-gear').show();
+                    setTimeout(function(){
+                        input_obj.prop('readonly', false);
+                        responseSec.removeClass('loading');
+                        resContainer.html('Available! <a href=\'#forms\' onclick=\'return false;\'>Order Now</a>');
+                        responseSec.addClass('success');
+                        $('.loading-gear').hide();
+                    }, 1000);
+                }
+                return false;
+            });
+        });
     });
 
 })(jQuery);
